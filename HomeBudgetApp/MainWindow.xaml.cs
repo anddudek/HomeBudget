413e84e99172ebec704432f326b772ca2de8bdb2
Andrzej Dudek <andrzej.dudek@wedzony.com.pl>
2017-08-03 12:56:39 +0200

    Navigation working

--- HomeBudgetApp/MainWindow.xaml.cs
+++ HomeBudgetApp/MainWindow.xaml.cs
@@ -27,7 +27,18 @@ namespace HomeBudgetApp
             InitializeComponent();
         }
 
+        private void HamburgerMenu_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            // instead using binding Content="{Binding RelativeSource={RelativeSource Self}, Mode=OneWay, Path=SelectedItem}"
+            // we can do this
+            this.HamburgerMenuControl.Content = e.ClickedItem;
 
+            // close the menu if a item was selected
+            if (this.HamburgerMenuControl.IsPaneOpen)
+            {
+                this.HamburgerMenuControl.IsPaneOpen = false;
+            }
+        }
 
         public void Use() { }
 
@@ -37,5 +48,22 @@ namespace HomeBudgetApp
         }
 
         public ICommand Lol { get { return new RelayCommand(Use, CanUse); } }
+    }
+
+    public class BindingProxy : Freezable
+    {
+        // Using a DependencyProperty as the backing store for Data. This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DataProperty = DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
+
+        public object Data
+        {
+            get { return (object)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
     }
 }
