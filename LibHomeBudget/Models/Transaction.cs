f8ce9381e5c224fb2e1925df8ceeb73a2b51696e
Andrzej Dudek <andrej.dudk@wp.pl>
2017-08-05 12:54:02 +0200

    EntityFramework

---
+++ LibHomeBudget/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibHomeBudget.Models
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public double Cost { get; set; }
+        public Guid CategoryId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
---
+++ LibHomeBudget/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibHomeBudget.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Hash { get; set; }
+        public bool HasSeenMessage { get; set; }
+    }
+}
